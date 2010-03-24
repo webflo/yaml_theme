d6f0d6bdad9791baa97129a7cb1ca5725f98cebd
root <root@debian.impressive.media>
2010-03-24 21:21:37 +0100

    removing clearfix from block

--- templates/block/block.tpl.php
+++ templates/block/block.tpl.php
@@ -1,4 +1,4 @@
- <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module; ?> <?php print $block->css?> clearfix" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
+ <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module; ?> <?php print $block->css?> " id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
 	    <?php if(!empty($block->subject)):?> <h2 class="title"><?php print $block->subject; ?></h2><?php endif; ?>
 	    <div class="content"><?php print $block->content; ?></div>
  </div>
